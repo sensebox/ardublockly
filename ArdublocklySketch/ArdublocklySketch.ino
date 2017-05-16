5a4a4fd927104e44458b08d3645642036d9a5c65
Bernd90 <webaccout@web.de>
2017-05-16 19:34:31 +0200

    bug fix

--- ArdublocklySketch/ArdublocklySketch.ino
+++ ArdublocklySketch/ArdublocklySketch.ino
@@ -1,41 +1,31 @@
 #include <SenseBox.h>
 
 RV8523 rtc;
-VEML6070 uv_sensor;
-
-TSL45315 lux_sensor;
-
 HDC100X hdc;
-
-BMP280 bmp_sensor;
-
-#include <Adafruit_NeoPixel.h>
- Adafruit_NeoPixel rgb_led_8 = Adafruit_NeoPixel(1,8,NEO_GRB + NEO_KHZ800);
-
+VEML6070 uv_sensor;
+TSL45315 lux_sensor;
 
 void setup() {
   //Setup Serial Print
   Serial.begin(9600);
 
-  rtc.set(10, 24, 8, 20, 4, 2016); // 08:24:10 20.04.2016
-  rtc.begin();
-  uv_sensor.begin();
-  bmp_sensor.begin();
+   rtc.begin();
+ rtc.setTime(__DATE__,__TIME__);
   hdc.begin();
-  rgb_led_8.begin();
+
+  uv_sensor.begin();
+  lux_sensor.begin();
 }
 
 void loop() {
-  Serial.print((String("" + (String) rtc.getYear() + "." + (String) rtc.getMonth() + "." + (String) rtc.getDay() + "  " + (String) rtc.getHour() + ":" + (String) rtc.getMin()+ ":" + (String) rtc.getSec()) + String("UV")));
-  Serial.println(uv_sensor.getUV());
-  Serial.print((String("" + (String) rtc.getHour() + ":" + (String) rtc.getMin() + ":" + (String) rtc.getSec()) + String("Lux")));
-  Serial.println(lux_sensor.getLux());
-  Serial.print((String("" + (String) rtc.getHour() + ":" + (String) rtc.getMin() + ":" + (String) rtc.getSec()) + String("Temperatur")));
+  Serial.print("Messung vom:  ");
+  Serial.println("" + (String) rtc.getYear() + "." + (String) rtc.getMonth() + "." + (String) rtc.getDay() + "  " + (String) rtc.getHour() + ":" + (String) rtc.getMin()+ ":" + (String) rtc.getSec());
+  Serial.print("Temperatur:  ");
   Serial.println(hdc.getTemp());
-  Serial.print((String("" + (String) rtc.getYear() + "." + (String) rtc.getMonth() + "." + (String) rtc.getDay() + "  " + (String) rtc.getHour() + ":" + (String) rtc.getMin()+ ":" + (String) rtc.getSec()) + String("Luftfeuchte")));
-  Serial.println(hdc.getHumi());
-  Serial.print((String("" + (String) rtc.getYear() + "." + (String) rtc.getMonth(); + "." + (String) rtc.getDay()) + String("Luftdruck")));
-  Serial.println(bmp_sensor.getPressure());
-  rgb_led_8.setPixelColor(0,rgb_led_8.Color(255,0,613));
-  rgb_led_8.show();
+  Serial.print("Uv Wert:  ");
+  Serial.println(uv_sensor.getUV());
+  Serial.print("Helligkeit:  ");
+  Serial.println(lux_sensor.getLux());
+  delay(1000);
+
 }
